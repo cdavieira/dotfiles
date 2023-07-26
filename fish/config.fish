90455c8f08199223f70c78481f06a04008897175
Carlos Daniel Albertino Vieira <76596868+paisdegales@users.noreply.github.com>
2023-07-26 00:03:31 -0300

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	if test -d "/home/carlosvieira/.cargo/bin"
+		set -x PATH $PATH /home/carlosvieira/.cargo/bin
+	end
 end

a65c304796e9ab095b02650f91b7c4e30b178f5d
paisdegales <cd_vieira@hotmail.com>
2023-08-13 18:29:22 -0300

    :sparkles: fish config updated

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	if test -d "/home/carlosvieira/.cargo/bin"
+	if test -d "$HOME/.cargo/bin"
 		set -x PATH $PATH /home/carlosvieira/.cargo/bin
+	end
+
+	if test -f "$HOME/.lynx/lynx.cfg"
+		set -x LYNX_CFG "/home/carlosvieira/.lynx/lynx.cfg"
 	end
 end

425074fa8fb1d3a0384bee49a679c5a8d8f6bd5a
paisdegales <cd_vieira@hotmail.com>
2023-09-10 23:28:54 -0300

    :pencil:

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 	if test -d "$HOME/.cargo/bin"
-		set -x PATH $PATH /home/carlosvieira/.cargo/bin
+		set -x PATH $PATH $HOME/.cargo/bin
 	end
 
 	if test -f "$HOME/.lynx/lynx.cfg"
-		set -x LYNX_CFG "/home/carlosvieira/.lynx/lynx.cfg"
-	end
-
-	if test -f "$HOME/.lynx/lynx.cfg"
+		set -x LYNX_CFG "$HOME/.lynx/lynx.cfg"
 		set -x LYNX_LSS "bright-blue.lss"
 	end
 end

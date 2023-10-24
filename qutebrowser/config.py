4432382a46949270cdafe7bd0c68b30bf0486780
paisdegales <cd_vieira@hotmail.com>
2023-10-24 14:16:50 -0300

    :pencil: qutebrowser op

---
+++ qutebrowser/config.py
@@ -0,0 +1,13 @@
+config.load_autoconfig()
+
+c.downloads.location.directory = "~/temp/"
+
+c.url.start_pages = ["https://www.qutebrowser.org/"]
+c.url.start_pages.extend([
+    "https://duckduckgo.com",
+    "https://qutebrowser.org/doc/help/configuring.html#configpy",
+    "https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html", 
+    "https://www.lua.org/manual/5.4/",
+    "https://archlinux.org/",
+    "https://wiki.archlinux.org/"
+])

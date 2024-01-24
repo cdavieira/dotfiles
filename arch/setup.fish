ddf529176849a4b5cbc1a6d3521f3ed89236ad95
paisdegales <cd_vieira@hotmail.com>
2024-01-23 21:20:45 -0300

    :pencil: minor updates

--- arch/setup.fish
+++ arch/setup.fish
@@ -4,17 +4,16 @@ function graphics -d "Install nvidia and intel drivers"
 	sudo pacman -S mesa vulkan-intel
 end
 
-function wayland -d "Install wayland, dwl and display manager"
+function wayland -d "Install wayland, dwl and a display manager"
 	sudo pacman -S wayland xorg-xwayland git make bear polkit pkg-config wlroots wayland-protocols bemenu-wayland waybar ly
+	git clone https://codeberg.org/dwl/dwl.git
+	ln -s ~/dotfiles/dwl/config.h -t ~/dwl -v
 	#nvim /etc/ly/config.ini
 	sudo systemctl enable ly
 end
 
 function fonts -d "Install nerd fonts"
 	sudo pacman -S ttf-{anonymouspro,firacode,hack}-nerd ttf-nerd-fonts-symbols{,-mono}
-
-	git clone https://codeberg.org/dwl/dwl.git
-	ln -s ~/dotfiles/dwl/config.h -t ~/dwl -v
 end
 
 function vim -d "Install vim and neovim"
@@ -29,7 +28,6 @@ end
 
 function shell -d "Install kitty and setup fish"
 	sudo pacman -S kitty
-
 	rm ~/.config/fish/config.fish
 	ln -s ~/dotfiles/fish/config.fish -t ~/.config/fish -v
 	ln -s ~/dotfiles/kitty/ -t ~/.config -v
@@ -37,7 +35,6 @@ end
 
 function utils -d "Install Misc"
 	sudo pacman -S firefox gcc gdb valgrind file which ffmpeg wget usbutils unzip unrar tree lshw os-prober efibootmgr ntfs-3g tmux
-
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 	ln -s ~/dotfiles/tmux/.tmux.conf -t ~ -v
 end

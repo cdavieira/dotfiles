47ede7b34b7549e640b3db19e922e20394117f03
paisdegales <cd_vieira@hotmail.com>
2024-02-11 23:13:17 -0300

    :tada:

--- dwl/config.h
+++ dwl/config.h
@@ -122,13 +122,15 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 static const char *termcmd[] = { "kitty", NULL };
 static const char *menucmd[] = { "bemenu-run", NULL };
 static const char *waybarcmd[] = { "waybar", NULL };
+static const char *browsercmd[] = { "qutebrowser", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
 	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,     spawn,          {.v = termcmd} },
-	{ MODKEY,                    XKB_KEY_b,          spawn,          {.v = waybarcmd} },
+	{ MODKEY,                    XKB_KEY_a,          spawn,          {.v = waybarcmd} },
+	{ MODKEY,                    XKB_KEY_b,          spawn,          {.v = browsercmd} },
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
 	{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,8 +1,17 @@
 config.load_autoconfig()
 
+#c.content.blocking.enabled = False
+#c.url.default_page = "https://start.duckduckgo.com/"
+#c.fonts.default_size = "10pt" # qutebrowser fontsize
+#c.fonts.web.size.default = 16 # webpage fontsize
+#c.colors.webpage.darkmode.enabled = True
+#c.colors.webpage.darkmode.threshold.foreground = 120
+#c.colors.webpage.darkmode.threshold.background = 255
+#c.colors.webpage.darkmode.algorithm = "lightness-hsl"
+c.colors.webpage.preferred_color_scheme = "dark"
 c.auto_save.session = True
 c.downloads.location.directory = "~/temp/"
-c.editor.command = ["vim", "{}"]
+c.editor.command = ["kitty", "--config", "/home/carlos/.config/kitty/kitty.conf", "-c", "\"fish", "nvim", "{}", "\""]
 c.url.searchengines = {
     "DEFAULT": "https://google.com/search?hl=en&q={}",
     #:open dd something-something
@@ -11,73 +20,73 @@ c.url.searchengines = {
     "y": "https://www.youtube.com/results?search_query={}",
     #:open w something-something
     "w": "https://en.wikipedia.org/wiki/{}",
-    #:open ig something-something
-    "i": "https://www.instagram.com/explore/tags/{}",
     #:open ge something-something
     "v": "https://www.verbformen.com/?w={}",
     #:open t something-something
     "t": "https://translate.google.com/?hl=pt-BR&sl=en&tl=de&text={}&op=translate",
+    #:open ig something-something
+    # "i": "https://www.instagram.com/explore/tags/{}",
 }
-#c.colors.webpage.darkmode.enabled = True
-#c.content.blocking.enabled = False
+
 #config.bind('A', 'spawn zathura {url}', mode='normal')
 #config.unbind('A', 'normal')
 
-start_pages = {
-    0: [
-        "https://duckduckgo.com",
+general = {
+    "vim": [
+        "https://www.vim.org/",
+        "https://neovim.io/",
     ],
-    1: [
+    "arch": [
         "https://archlinux.org/",
         "https://wiki.archlinux.org/",
     ],
-    2: [
+    "de": [
+        "https://translate.google.com/?sl=en&tl=de&op=translate",
+        "https://www.verbformen.com/",
+    ],
+    "misc": [
         "https://sw.kovidgoyal.net/kitty/",
         "https://fishshell.com/",
         "https://www.qutebrowser.org/",
-        "https://github.com/djpohly/dwl",
-        "https://awesomewm.org/",
-        "https://neovim.io/",
+        "https://git.pwmt.org/pwmt/zathura",
+        "https://sr.ht/~exec64/imv/",
+        "https://github.com/mpv-player/mpv",
     ],
-    3: [
-        "https://sw.kovidgoyal.net/kitty/overview/",
-        "https://fishshell.com/docs/current/index.html",
-        "https://qutebrowser.org/doc/help/settings.html",
-        "https://dwm.suckless.org/customisation/",
-        "https://github.com/djpohly/dwl/wiki",
-        "https://awesomewm.org/apidoc/index.html",
-        "https://neovim.io/doc/user/vim_diff.html#nvim-features",
-        "https://neovim.io/doc/user/lua.html",
+    "dwl": [
+        "https://codeberg.org/dwl/dwl",
+        "https://wayland.freedesktop.org/docs/html/index.html",
+        "https://wayland.app/protocols/",
     ],
-    4: [
-        "https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html", 
+}
+
+code = {
+    "cpp": {
         "https://en.cppreference.com/w/",
-        "https://www.lua.org/manual/5.4/",
-    ],
-    5: {
-        "https://www.typescriptlang.org/docs/handbook/intro.html",
-        "https://react.dev/learn",
+        "https://cplusplus.com/",
     },
-    6: {
+    "rust": [
+        "https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html", 
+    ],
+    "lua": {
+        "https://www.lua.org/manual/5.4/",
+    },
+    "js": {
         "https://developer.mozilla.org/en-US/docs/Web/javascript",
-        "https://en.wikipedia.org/wiki/JSX_(JavaScript)",
         "https://www.w3schools.com/Js/js_es6.asp",
         "https://www.w3schools.com/js/js_htmldom.asp",
         "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise",
     },
-    7: {
-        "https://wiki.archlinux.org/title/NVIDIA",
-        "https://wiki.archlinux.org/title/Nouveau",
-        "https://wiki.archlinux.org/title/NVIDIA_Optimus",
+    "ts": {
+        "https://www.typescriptlang.org/docs/handbook/intro.html",
     },
-    8: {
-        #
-    }
+    "react": {
+        "https://react.dev/learn",
+        "https://en.wikipedia.org/wiki/JSX_(JavaScript)",
+    },
 }
 
 c.url.start_pages = [
     "https://chat.openai.com/"
-    #"https://raw.githubusercontent.com/qutebrowser/qutebrowser/main/doc/img/cheatsheet-big.png"
 ]
 
-c.url.start_pages.extend(start_pages[4])
+c.url.start_pages.extend(general["arch"])

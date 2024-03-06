f5be4887ff05b56130a0e6f2d84b1c18f909c4f5
paisdegales <cd_vieira@hotmail.com>
2024-03-06 01:43:37 -0300

    :tada:

--- fish/config.fish
+++ fish/config.fish
@@ -33,6 +33,8 @@ if status is-interactive
 	abbr --add dotdot --position command --regex '^\.\.+$' --function multicd
 
 	alias ls 'ls --group-directories-first --color=auto'
+
+	set LANG 'de_DE.UTF-8'
 else if status is-login
 	# Commands to run in login sessions can go here
 end

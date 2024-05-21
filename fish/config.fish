45535695fbf75ce7d64612f67241385789812c76
paisdegales <cd_vieira@hotmail.com>
2024-05-21 10:16:04 -0300

    :tada:

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
 if status is-interactive
 	# Commands to run in interactive sessions can go here
 
-	# Static variables
+	#######################################
+	########## Static variables ###########
+	#######################################
+
 	# set LANG 'de_DE.UTF-8'
 	set -x GMAIL 'cd.vieira14@gmail.com'
 	set -x OUTLOOK 'cd_vieira@hotmail.com'
@@ -28,15 +31,20 @@ if status is-interactive
 	# set -x XDG_DATA_DIRS "/usr/local/share:/usr/share"
 	# set -x XDG_CONFIG_DIRS "/etc/xdg"
 
-	# Dynamic variables
+	#######################################
+	######### Dynamic variables ###########
+	#######################################
+	# add rust package manager's 'bin/' to path
 	if test -d "$HOME/.cargo/bin"
 		set -x PATH $PATH "$HOME/.cargo/bin"
 	end
 
+	# add '~/.local/bin/' to path
 	if test -d "$HOME/.local/bin"
 		set -x PATH $PATH "$HOME/.local/bin"
 	end
 
+	# set EDITOR env var
 	set editors 'vim' 'nvim' 'less'
 	for editor in $editors
 		if type -q $editor
@@ -45,21 +53,28 @@ if status is-interactive
 		end
 	end
 
+	# in case ranger is installed, skip loading its default rc
 	if type -q 'ranger'
 		set -x RANGER_LOAD_DEFAULT_RC FALSE
 	end
 
+	# in case neovim is installed, use it as pager
 	if type -q 'nvim'
 		set -x MANPAGER 'nvim +Man!'
 	end
 
+	# in case qutebrowser is installed, use it as def browser
 	if type -q 'qutebrowser'
 		set -x BROWSER 'qutebrowser'
 	end
 
-	# Aliases
+	#######################################
+	############## Aliases ################
+	#######################################
 	alias ls 'ls --group-directories-first --color=auto'
 	alias la 'ls --group-directories-first --color=auto -la'
+	
+	# shortcut tmux to automatically open my most used folders
 	if type -q 'tmux'
 		alias tmux "tmux new-session -c ~/dotfiles -n dotfiles \; \
 			new-window -c ~/notes -n notes \; \
@@ -67,8 +82,11 @@ if status is-interactive
 			new-window -c ~/code/c/projects/gwl_keys -n gwl_keys \; "
 	end
 
-	# Abbreviations
-	#abbr --position anywhere dotfiles vim $HOME/dotfiles
+	#######################################
+	########### Abbreviations #############
+	#######################################
+
+	# save some typing
 	if type -q 'ranger'
 		abbr -a do ranger $HOME/dotfiles
 		abbr -a no ranger $HOME/notes
@@ -79,12 +97,17 @@ if status is-interactive
 		abbr -a co cd $HOME/code
 	end
 
-	# Functions
+	#######################################
+	############# Functions ###############
+	#######################################
+
+	# use '......' idiom to move to any directory level up
 	function multicd
 			echo (string repeat -n (math (string length -- $argv[1]) - 1) ../)
 	end
 	abbr --add dotdot --position command --regex '^\.\.+$' --function multicd
 
+	# copy the old cd command from fish and enhance it with ls
 	functions -c cd oldcd
 	function cd
 		oldcd $argv

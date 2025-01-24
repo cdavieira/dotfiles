34e5f243cce2cdc10a7c02ea5cc78bdbd7bb3a37
cdavieira <cd_vieira@hotmail.com>
2025-01-24 14:42:09 -0300

    :tada:

--- gentoo/init.sh
+++ gentoo/init.sh
@@ -9,7 +9,7 @@ fi
 if test ! -d "${XDG_RUNTIME_DIR}"; then
 	sudo mkdir --parents ${XDG_RUNTIME_DIR}
 	sudo chown carlos ${XDG_RUNTIME_DIR}
-	#sudo chmod 0700 ${XDG_RUNTIME_DIR}
+	sudo chmod 0700 ${XDG_RUNTIME_DIR}
 else
 	echo "'${XDG_RUNTIME_DIR}' already exists"
 fi

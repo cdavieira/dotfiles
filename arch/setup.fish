79805005a5fdfb92a94965c3961506f0ae627fb4
cdavieira <cd_vieira@hotmail.com>
2025-02-04 00:51:35 -0300

    :tada:

--- arch/setup.fish
+++ arch/setup.fish
@@ -8,8 +8,8 @@ function wayland -d "Install wayland, dwl and a display manager"
 	sudo pacman -S wayland xorg-xwayland git make bear polkit pkg-config wlroots \
 	wayland-protocols bemenu-wayland waybar ly wl-clipboard wf-recorder slurp \
 	grim swappy fnott swaybg wl-mirror bemenu-wayland
-	git clone https://codeberg.org/dwl/dwl.git
-	ln -s ~/dotfiles/dwl/config.h -t ~/dwl -v
+	git clone https://codeberg.org/dwl/dwl.git ~/repos
+	ln -s ~/repos/dotfiles/dwl/config.h ~/repos/dwl
 	#nvim /etc/ly/config.ini
 	sudo systemctl enable ly
 end
@@ -20,27 +20,26 @@ end
 
 function vim -d "Install vim and neovim"
 	sudo pacman -S neovim nodejs python-pynvim fd ripgrep wl-clipboard clang {lua,typescript}-language-server pyright cmake
-	ln -s ~/dotfiles/nvim/ -t ~/.config -v
+	ln -s ~/repos/dotfiles/nvim/ -t ~/.config -v
 
 	sudo pacman -S vim
-	mkdir -p ~/.config/vim ~/.cache/vim/{backup,swap,undo}
-	curl -fLo ~/.config/vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	ln -s ~/dotfiles/vim/vimrc -t ~/.config/vim -v
+	ln -s ~/repos/dotfiles/vim/vimrc ~/.config/vim -v
 end
 
 function shell -d "Install kitty and setup fish"
 	sudo pacman -S kitty
 	rm ~/.config/fish/config.fish
-	ln -s ~/dotfiles/fish/config.fish -t ~/.config/fish -v
-	ln -s ~/dotfiles/fish/functions/ -t ~/.config/fish -v
-	ln -s ~/dotfiles/kitty/ -t ~/.config -v
+	ln -s ~/repos/dotfiles/fish/config.fish ~/.config/fish
+	ln -s ~/repos/dotfiles/fish/functions/ ~/.config/fish
+	ln -s ~/repos/dotfiles/kitty/ ~/.config
 end
 
 function utils -d "Install Misc"
-	sudo pacman -S qutebrowser gcc gdb valgrind file which ffmpeg wget usbutils unzip unrar tree lshw os-prober efibootmgr ntfs-3g tmux
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	mkdir ~/.config/tmux
-	ln -s ~/dotfiles/tmux/tmux.conf -t ~/.config/tmux/tmux.conf -v
+	sudo pacman -S qutebrowser gcc gdb valgrind file which ffmpeg \
+	wget usbutils unzip unrar tree lshw os-prober efibootmgr ntfs-3g tmux
+	mkdir ~/.config/tmux/{,plugins}
+	git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+	ln -s ~/repos/dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
 end
 
 function audio -d "Setup pipewire and audio dependencies"
--- dwl/startup.sh
+++ dwl/startup.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 # <&- closes standard input
+
 gentoo-pipewire-launcher <&- &
+
 waybar <&- &
+
 #swaybg -i ~/repos/dotfiles/dwl/background.png -m fit <&- &
 swaybg -i ~/repos/dotfiles/dwl/orangutan.png -m fit <&- &
+
 dunst <&- &

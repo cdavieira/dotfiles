63c497dc1f19ecb4355f96caef695f788001c096
cdavieira <cd_vieira@hotmail.com>
2025-02-05 15:52:43 -0300

    :tada:

--- dwl/startup.fish
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/fish
-
-# <&- closes standard input
-waybar <&- &
-swaybg -i ~/repos/dotfiles/dwl/background.png -m fit <&- &
--- dwl/startup.sh
+++ dwl/startup.sh
@@ -2,11 +2,44 @@
 
 # <&- closes standard input
 
-gentoo-pipewire-launcher <&- &
+# https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+# command --help
+command_exists(){
+	command -v $1 &>/dev/null
+	return 
+}
 
-waybar <&- &
+mywallpaper="$HOME/repos/dotfiles/wallpapers/background.png"
 
-#swaybg -i ~/repos/dotfiles/dwl/background.png -m fit <&- &
-swaybg -i ~/repos/dotfiles/dwl/orangutan.png -m fit <&- &
+if command_exists gentoo-pipewire-launcher; then
+  gentoo-pipewire-launcher <&- &
+else
+  echo "dwl-startup.sh: fail when starting pipewire (gentoo)"
+fi
 
-dunst <&- &
+if command_exists waybar; then
+  waybar <&- &
+else
+  echo "dwl-startup.sh: fail when starting bar (waybar not found)"
+fi
+
+if test -e ${mywallpaper}; then
+  swaybg -i ${mywallpaper} -m fit <&- &
+else
+  echo "dwl-startup.sh: fail when setting background (swaybg not found)"
+fi
+
+if command_exists dunst; then
+  dunst <&- &
+else
+  echo "dwl-startup.sh: fail when starting notify-daemon (dunst not found)"
+fi
+
+if command_exists swayidle && command_exists swaylock && command_exists wlopm; then
+  swayidle -w \
+    timeout 300 'swaylock -f -c 000000' \
+    timeout 600 'wlopm --off eDP-1' \
+    resume 'wlopm --on eDP-1'
+else
+  echo "dwl-startup.sh: fail when starting screensaving service (swayidle/swaylock/wlopm not found)"
+fi

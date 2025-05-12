46a0fcd4d024158c46352c1139fc52984c183da6
cdavieira <cd_vieira@hotmail.com>
2025-05-12 00:26:14 -0300

    :tada:

---
+++ greetd/dwl.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+DWL_PID=`pgrep dwl`
+#gtkgreet && kill $DWL_PID
+regreet && kill $DWL_PID
---
+++ greetd/guigreet.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if test -z "${XDG_RUNTIME_DIR}"; then
+	export XDG_RUNTIME_DIR="/run/user/${UID}"
+else
+	echo "XDG_RUNTIME_DIR is already set to ${XDG_RUNTIME_DIR}"
+fi
+
+if test ! -d "${XDG_RUNTIME_DIR}"; then
+	sudo mkdir --parents ${XDG_RUNTIME_DIR}
+	sudo chown carlos ${XDG_RUNTIME_DIR}
+	sudo chmod 0700 ${XDG_RUNTIME_DIR}
+else
+	echo "'${XDG_RUNTIME_DIR}' already exists"
+fi
+
+dbus-launch --exit-with-session dwl -s /etc/greetd/dwl.sh
